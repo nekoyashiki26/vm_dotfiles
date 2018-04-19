13e8172523b9d3239376ad7a2ef694f7636e4c89
YoshinoriYamaguchi <b1518292@planet.kanazawa-it.ac.jp>
2018-04-19 15:52:34 +0900

    changed

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -u
 
 # 実行場所のディレクトリを取得
@@ -13,10 +12,14 @@ echo "start setup..."
 for f in .??*; do
     [ "$f" = ".git" ] && continue
     [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".gitconfig" ] && continue
+    [ "$f" = ".gitignore" ] && continue
+    [ "$f" = ".DS_Store" ] && continue
     [ "$f" = ".require_oh-my-zsh" ] && continue
     [ "$f" = ".gitmodules" ] && continue
 
     ln -snfv "$THIS_DIR"/"$f" ~/
+    ln -snfv "$THIS_DIR"/"setproxy.sh" ~/
 done
 
 cat << END
